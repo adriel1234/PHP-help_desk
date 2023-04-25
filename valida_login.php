75dbd0191ff19d6d471d4bda025f3ddf88a38803
Adriel Vinicius <adriel1234.1234@gmail.com>
2023-04-25 18:57:16 -0400

    validando login em memoria

---
+++ valida_login.php
@@ -0,0 +1,30 @@
+<?php 
+
+$usuarios_app = array(
+array('email' => 'adm@teste.com.br','senha'=>'123456'),
+array('email' => 'user@teste.com.br','senha' => 'abcd')
+);
+/*
+echo '<pre>';
+print_r($usuarios_app);
+echo '</pre>';
+
+*/
+    //varia que verifica se a autenticacao foi realizada
+    $usuario_autenticado = false;
+
+foreach($usuarios_app as $user){
+
+    if($user['email'] == $_POST['email'] &&  $user['senha'] == $_POST['senha']){
+        $usuario_autenticado = true;
+    }
+}
+if($usuario_autenticado){
+    echo 'Usuário autenticado';
+
+}else{
+    header('Location: index.php?login=erro');
+}
+
+
+?>
