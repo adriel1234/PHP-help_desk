9a425ea131ed93e195953637f080c2584935fd80
Adriel Vinicius <adriel1234.1234@gmail.com>
2023-04-25 20:27:26 -0400

    resgistrando chamado arquivo.hd

---
+++ logoff.php
@@ -0,0 +1,23 @@
+<?php 
+
+session_start();
+
+/*
+echo '<pre>';
+print_r($_SESSION);
+echo '</pre>';
+
+unset($_SESSION['x']);
+
+
+
+
+echo '<pre>';
+print_r($_SESSION);
+echo '</pre>';
+*/
+
+session_destroy();
+header('Location: index.php');
+
+?>
